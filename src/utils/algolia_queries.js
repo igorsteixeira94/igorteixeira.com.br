0f2bf85f0309314de05ce8343ea1bf435a32e278
Igor Rodrigues <igorsteixeira94@gmail.com>
2020-09-16 09:30:36 -0300

    dotenv

--- src/utils/algolia_queries.js
+++ src/utils/algolia_queries.js
@@ -36,7 +36,7 @@ const queries = [
   {
     query: postsQuery,
     transformer: ({ data }) => flatten(data.posts.edges),
-    indexName: 'dev_POSTS', // overrides main index name, optional
+    indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME, // overrides main index name, optional
     setSettings: {
       atributesToSnippet: ['excerpt:20'],
     },
