6b874876b4fe268dfd908e8534d727e61f34ea8a
Igor Rodrigues <igorsteixeira94@gmail.com>
2020-10-02 17:52:40 -0300

    chore: lint text

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 import * as S from '../components/Post/styles';
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,6 @@ import Layout from '../components/Layout';
 import SEO from '../components/seo';
 
 import * as S from '../components/Post/styles';
-import ImageNote from '../images/notes.jpg';
 import SocialLinks from '../components/SocialLinks';
 
 function AboutPage() {
@@ -15,15 +14,11 @@ function AboutPage() {
       />
       <S.MainContent>
         <h1 style={{ margin: 0 }}>Sobre mim</h1>
-
         <p>
-          Meu nome Igor Rodrigues da Silva Teixeira, nasci em Barreiras/Ba. Sou
-          Cientista da Computação e criei esse blog para contar um pouco da
-          minha jornada no mundo da programação.
+          Meu nome Igor Rodrigues da Silva Teixeira, nasci em Barreiras-Ba, sou
+          cientista da computação e apaixonado por tecnologia.
         </p>
-
-        <h2>Um breve histórico do meu contato com programação</h2>
-
+        <h3>Como tudo começou</h3>
         <p>
           O meu primeiro contato com programação foi durante 2009-2012, no curso
           técnico do IFBA, onde tive contato com linguagens como: "Portugol, C,
@@ -43,28 +38,10 @@ function AboutPage() {
               (utilizei no meu tcc)
             </a>
           </small>
-          , engenharia de software, entre outras que construiram uma boa lógica
-          de programação. Também aprendi muitas 'soft skills', como : aprender a
-          aprender, ensinar, ter boa comunicação, capacidade de resolver
-          conflitos, ... . Além disso, pude conhecer muitas pessoas e fazer
-          grandes amizades.
-        </p>
-        <p>
-          Logo após a graduação também tive a oportunidade de trabalhar como{' '}
-          <strong>Desenvolvedor PHP</strong> utilizando o Adianti Framework.
-          Depois de um ano trabalhando com php resolvi investir no JavaScript e
-          seu ecossistema, foi amor a primeira vista!
+          , engenharia de software, ... . Desde então estou sempre consumindo
+          conteúdo e aprendendo coisas novas.
         </p>
 
-        <img src={ImageNote} alt="Resumos em papel" className="classImg" />
-        <p>
-          Esses são alguns resumos que faço quando estou aprendendo sobre
-          qualquer coisa. Não importa se estou consumindo uma documentação, um
-          curso, um vídeo do youtube ou um artigo, tenho que documentar esse
-          aprendizado em anotações e resumos. Então resolvi criar esse blog para
-          "digitalizar" minha anotações. Assim, continuo aprendendo e
-          contribuindo para quem deseja aprender também.
-        </p>
         <blockquote>
           <p>
             "Quem ensina aprende ao ensinar. E quem aprende ensina ao aprender."{' '}
@@ -79,8 +56,8 @@ function AboutPage() {
           sobre finanças.
         </p>
         <p>
-          Se perdeu todo esse tempo lendo sobre mim, entre em contato através de
-          qualquer uma das minhas redes sociais.
+          <del>Se perdeu todo esse tempo lendo sobre mim</del>, entre em contato
+          através de qualquer uma das minhas redes sociais.
         </p>
         <SocialLinks className="socialLinkAbout" />
       </S.MainContent>
